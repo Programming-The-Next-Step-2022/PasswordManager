e357e8d8b4cc75ede356ed3f3da0f480a11d94ee
nebnebb <58271113+nebnebb@users.noreply.github.com>
2022-05-20 17:38:00 +0200

    Update Encryption.py

--- src/Encryption.py
+++ src/Encryption.py
@@ -10,24 +10,44 @@ class Encryption(object):
         self.key = hashlib.sha256(key.encode()).digest()
         self.blocksize = AES.block_size
 
-    def pad(self, s):
-        return s + (self.blocksize - len(s) % self.blocksize) * chr(
-            self.blocksize - len(s) % self.blocksize)
+    def pad(self, string):
+        """
+        Pad the password string until it is a multiple of 16
 
-    def unpad(self, s):
-        return s[:-ord(s[len(s) - 1:])]
+        :param string: str containing the password
+        :return: padded password
+        """
+        return string + (self.blocksize - len(string) % self.blocksize) * chr(
+            self.blocksize - len(string) % self.blocksize)
+
+    def unpad(self, string):
+        """
+        Remove the padding
+        :param string: padded string
+        :return: unpadded string
+        """
+        return string[:-ord(string[len(string) - 1:])]
 
     def encrypt(self, password):
+        """
+        Encrypt the password according to AES by using the key
+        :param password: str containing the password
+        :return: encrypted password
+        """
         padded_password = self.pad(password)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(padded_password.encode()))
 
     def decrypt(self, encoded_password):
+        """
+        Decrypt the AES-encrypted password using the key
+        :param encoded_password: encoded and padded password
+        :return: decoded and unpadded password 
+        """
         encoded_password = base64.b64decode(encoded_password)
         iv = encoded_password[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        # return self.unpad(cipher.decrypt(encoded_password[self.bs:]))
         return self.unpad(cipher.decrypt(encoded_password[AES.block_size:])).decode('utf-8')
 
 
